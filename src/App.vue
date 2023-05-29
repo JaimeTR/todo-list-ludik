6c806194855b8ca510e078473c74789d5728b9e5
Jaime Tarazona ✅ <38092883+JaimeTR@users.noreply.github.com>
2023-05-29 16:53:03 -0500

    v0.1.0
    Avance de Todo-List de LUDIK

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task.vue";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Aprendo Vue.js",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Trabajo en Ludik",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Me voy de paseo",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Hago la tarea",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Envio correos",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
